5689a2f8e88ee06550090a5597d671c8889cccc1
dagz77 <20237877@s.ubaguio.edu>
2024-10-09 13:08:18 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //tag used to include this compnent in HTML
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
